8346406cef7378c7a30839f116156ecff22587a3
leeight <leeight@gmail.com>
2016-04-22 16:15:02 +0800

    更新 demo/doc.js

--- demo/doc.js
+++ demo/doc.js
@@ -19,6 +19,8 @@ function toUrl(object) {
 var AK = getQuery('ak', 'f1a2705d3cf8448cb917684c4f40ac1f');
 var SK = getQuery('sk', '5fd876eb57834c2f8156d8e65890d0fd');
 var DOC_ENDPOINT = getQuery('doc.endpoint', 'http://doc.bce-testinternal.baidu.com');
+var BOS_ENDPOINT = getQuery('bos.endpoint', 'http://bos.qasandbox.bcetest.baidu.com');
+var BOS_BUCKET = getQuery('bos.bucket', 'bkt-gavfz4jzhjtq0y4c');
 var CHUNK_SIZE = '1m';
 
 var doc = new baidubce.sdk.DocClient.Document({
@@ -27,68 +29,21 @@ var doc = new baidubce.sdk.DocClient.Document({
 });
 
 function getDocKey(file) {
-  var localKey = [AK, file.name, file.size, CHUNK_SIZE].join('&');
-  var localValue = localStorage.getItem(localKey);
-  if (!localValue) {
-    // return baidubce.utils.md5sum(file)
-    return baidubce.sdk.Q.resolve('deedb8a3acd52223f6dcd7d526e9e650')
-      .then(function (md5) {
-        var options = {
-          meta: {
-            sizeInBytes: file.size,
-            md5: md5,
-          },
-          title: file.name,
-          format: file.name.split('.').pop()
-        };
-        return doc.register(options)
-      })
-      .then(function (response) {
-        var documentId = response.body.documentId;
-        var bucket = response.body.bucket;
-        var key = response.body.object;
-        var bosEndpoint = response.body.bosEndpoint;
+  // source/doc-gdxink1qakahwu6k.txt
+  var format = file.name.split('.').pop();
+  var name = (Math.random() * Math.pow(2, 32)).toString(36);
+  var object = 'source/doc-' + name + '.' + format;
 
-        uploader.client.config.endpoint = bosEndpoint;
-        file.__documentId = documentId;
-        file.__bucket = bucket;
-        file.__object = key;
+  file.__object = object;
 
-        localStorage.setItem(localKey, JSON.stringify(response.body));
-
-        return {
-          bucket: bucket,
-          key: key
-        }
-      })['catch'](function (error) {
-        uploader._invoke('Error', [error, uploader._currentFile]);
-        uploader._uploadNext();
-        throw error;
-      });
-  }
-  else {
-    localValue = JSON.parse(localValue);
-
-    var documentId = localValue.documentId;
-    var bosEndpoint = localValue.bosEndpoint;
-    var bucket = localValue.bucket;
-    var key = localValue.object;
-
-    uploader.client.config.endpoint = bosEndpoint;
-    file.__documentId = documentId;
-    file.__bucket = bucket;
-    file.__object = key;
-
-    return {
-      bucket: bucket,
-      key: key
-    };
-  }
+  return object;
 }
 
 var uploader = new baidubce.bos.Uploader({
   browse_button: '#file',
   multi_selection: true,
+  bos_bucket: BOS_BUCKET,
+  bos_endpoint: BOS_ENDPOINT,
   bos_ak: AK,
   bos_sk: SK,
   auto_start: false,
@@ -98,6 +53,7 @@ var uploader = new baidubce.bos.Uploader({
   bos_multipart_parallel: 1,
   chunk_size: CHUNK_SIZE,
   dir_selection: false,
+  accept: 'txt,pdf,doc,docx,ppt,pptx,xls,xlsx',
   flash_swf_url: '../bower_components/moxie/bin/flash/Moxie.swf',
   init: {
     FilesFilter: function (_, files) {
@@ -126,14 +82,11 @@ var uploader = new baidubce.bos.Uploader({
       row.setUrl(url);
       row.setTime(time);
 
-      var localKey = [AK, file.name, file.size, CHUNK_SIZE].join('&');
-      localStorage.removeItem(localKey);
-
-      doc.publish(file.__documentId)
-        .then(function () {
-          row.setDocumentId(file.__documentId);
+      doc.createFromBos(BOS_BUCKET, file.__object, file.name)
+        .then(function (response) {
+          row.setMediaId(response.body.documentId);
         })['catch'](function (error) {
-          row.setDocumentId(String(error));
+          row.setMediaId(String(error));
         });
     },
     UploadComplete: function () {
